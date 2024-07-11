db4ca7091a90c40c1f620b9604e90d58eb45fcbb
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-07-11 17:41:28 -0500

    Updated backend server files

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require('express').Router()
+const { v4: uuidv4 } = require('uuid')
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils')
+
+// GET Route for retrieving all notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+})
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    console.log(req.body)
+
+    const { title, text } = req.body
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        }
+
+        readAndAppend(newNote, './db/db.json')
+        res.json('Note added successfully')
+    } else {
+        res.errored('Error in adding note')
+    }
+})
+
+module.exports = notes
